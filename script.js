badb6f1070bf34384d3ea436bb23af2bc6b9d1a0
Jaunty <jaunty5899@gmail.com>
2023-03-22 16:22:38 +0530

    fixed input toggle

--- script.js
+++ script.js
@@ -3,8 +3,11 @@ const search_mini = search_box.querySelector("#search_mini");
 const search_input = search_box.querySelector("#search_input");
 
 search_mini.addEventListener("click", () => {
-  search_box.classList.toggle("expand");
-  if (search_input.style.display == "none")
+  if (search_input.style.display == "none") {
     search_input.style.display = "block";
-  else search_input.style.display = "none";
+    search_box.classList.add("expand");
+  } else {
+    search_input.style.display = "none";
+    search_box.classList.remove("expand");
+  }
 });
