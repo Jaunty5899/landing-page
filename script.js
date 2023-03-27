cf05c3ede958ec2d430a5f7f5d2c2010bcdfb1ef
Jaunty <jaunty5899@gmail.com>
2023-03-27 11:24:44 +0530

    js code reduced

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
 const search_box = document.querySelector(".search_box");
-const search_mini = search_box.querySelector("#search_mini");
-const search_input = search_box.querySelector("#search_input");
+const search_mini = search_box.querySelector(".search_mini");
+const search_input = search_box.querySelector(".search_input");
 
 search_mini.addEventListener("click", () => {
-  if (search_input.style.display == "none") {
-    search_box.style.width = "600px";
-    search_input.style.display = "block";
-  } else {
-    search_input.style.display = "none";
-    search_box.style.width = "fit-content";
-  }
+  search_box.classList.toggle("expand");
+  search_input.classList.toggle("visible");
 });
