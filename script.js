c602988fb02ff1c886dd36042fa0cdfdf844477d
Jaunty <jaunty5899@gmail.com>
2023-03-27 13:59:08 +0530

    fixed transition

--- script.js
+++ script.js
@@ -4,5 +4,8 @@ const search_input = search_box.querySelector(".search_input");
 
 search_mini.addEventListener("click", () => {
   search_box.classList.toggle("expand");
-  search_input.classList.toggle("visible");
+  let timeout = !search_input.classList.contains("hide") ? 990 : 10;
+  setTimeout(() => {
+    search_input.classList.toggle("hide");
+  }, timeout);
 });
