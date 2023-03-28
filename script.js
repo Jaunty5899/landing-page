dded683970598248a4fa327ff7beb15201752edb
Jaunty <jaunty5899@gmail.com>
2023-03-28 10:37:26 +0530

    final version

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const search_box = document.querySelector(".search_box");
-const search_mini = search_box.querySelector(".search_mini");
+const search_btn = search_box.querySelector(".button");
 const search_input = search_box.querySelector(".search_input");
 
-search_mini.addEventListener("click", () => {
+search_btn.addEventListener("click", () => {
   search_box.classList.toggle("expand");
-  let timeout = !search_input.classList.contains("hide") ? 990 : 10;
+  let timeout = !search_input.classList.contains("hide")
+    ? 0.25 * 1000 - 10
+    : 10;
   setTimeout(() => {
     search_input.classList.toggle("hide");
   }, timeout);
