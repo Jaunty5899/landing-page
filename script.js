067ec3b283d6618f0d62f70fae054ebc6ed27134
Jaunty <jaunty5899@gmail.com>
2023-03-29 14:35:36 +0530

    some cleanup and additions

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const search_box = document.querySelector(".search_box");
 const search_btn = search_box.querySelector(".button");
 const search_input = search_box.querySelector(".search_input");
+const search_img = search_box.querySelector("img");
 
 search_btn.addEventListener("click", () => {
+  let timeout;
   search_box.classList.toggle("expand");
-  let timeout = !search_input.classList.contains("hide")
-    ? 0.25 * 1000 - 10
-    : 10;
+  if (!search_input.classList.contains("hide")) {
+    timeout = 0.2 * 1000 - 10;
+    search_img.src = "search.svg";
+  } else {
+    timeout = 10;
+    search_img.src = "cross.svg";
+  }
   setTimeout(() => {
     search_input.classList.toggle("hide");
   }, timeout);
