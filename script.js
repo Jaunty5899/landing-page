964607a78897a1a5d7e09f94766503762d956f3e
Jaunty <jaunty5899@gmail.com>
2023-04-01 19:23:35 +0530

    nav ready

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const search_box = document.querySelector(".search_box");
 const search_btn = search_box.querySelector(".button");
 const search_input = search_box.querySelector(".search_input");
-const search_img = search_box.querySelector("img");
 
 search_btn.addEventListener("click", () => {
-  let timeout;
+  let timeout, img_src;
   search_box.classList.toggle("expand");
   if (!search_input.classList.contains("hide")) {
     timeout = 0.2 * 1000 - 10;
-    search_img.src = "search.svg";
+    img_src = "search";
   } else {
     timeout = 10;
-    search_img.src = "cross.svg";
+    img_src = "cross";
   }
   setTimeout(() => {
+    search_btn.style.background = `center / 35% no-repeat url(${img_src}.svg)`;
     search_input.classList.toggle("hide");
   }, timeout);
 });
