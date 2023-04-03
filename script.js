917ffa1a5b500478e653187f7b8908e141c76d20
Jaunty <jaunty5899@gmail.com>
2023-04-03 10:19:02 +0530

    smol menu with fixed width

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const search_box = document.querySelector(".search_box");
 const search_btn = search_box.querySelector(".button");
 const search_input = search_box.querySelector(".search_input");
-const nav_logo = document.querySelector(".logo");
 const nav_menu = document.querySelector(".menu");
+const nav_btn = nav_menu.querySelector(".button");
+const ul = nav_menu.querySelector("ul");
+const logo = document.querySelector(".logo");
+let opaque;
+
+nav_btn.addEventListener("click", () => {
+  opaque = opaque ? 0 : 1;
+  ul.style.opacity = opaque;
+});
 
 search_btn.addEventListener("click", () => {
   let timeout, img_src;
+  logo.classList.toggle("contract");
   search_box.classList.toggle("expand");
   if (!search_input.classList.contains("hide")) {
     timeout = 0.2 * 1000 - 10;
